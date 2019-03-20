5d005287fddb91abe264197732af923c19e0026c
agachocz <agachocz@gmail.com>
2019-03-20 22:11:22 +0100

    Initialize

---
+++ optymalizacja_funkcji/optymalizacja_funkcji/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optymalizacja_funkcji
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            double result = RandomSearch.Run(300, rand);
+            Console.WriteLine("Random search: " + result);
+        
+
+            double result2 = HillClimbing.Run(300, rand);
+            Console.WriteLine("Hill climbing: " + result2);
+
+            double result3 = TabooSearch.Run(300, rand, 4);
+            Console.WriteLine("Taboo search: " + result3);
+            Console.ReadKey();
+        }
+    }
+}
